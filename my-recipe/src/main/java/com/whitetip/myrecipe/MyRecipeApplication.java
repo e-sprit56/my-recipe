5f04aa8fb481a5c0f62c931e69b0d7b43a203854
e-sprit56 <ksamburski@gmail.com>
2022-11-05 15:29:52 +0100

    Initial commit

---
+++ my-recipe/src/main/java/com/whitetip/myrecipe/MyRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.whitetip.myrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRecipeApplication.class, args);
+    }
+
+}
---
+++ my-recipe/src/test/java/com/whitetip/myrecipe/MyRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitetip.myrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
